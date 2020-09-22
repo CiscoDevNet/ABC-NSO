ba015dbb9912c2fe7d39677375ec423ca1eb1b33
Joe Clarke <jclarke@cisco.com>
2020-09-22 10:08:33 -0400

    Rename.

--- lab9/solution/cheat.sh
+++
@@ -1,4 +0,0 @@
-cp ~/solution/*.py ~/nso-lab9/packages/l3vpn/python/l3vpn/
-cp ~/solution/*.yang ~/nso-lab9/packages/l3vpn/src/yang/
-make -C ~/nso-lab9/packages/l3vpn/src
-echo 'packages reload' | ncs_cli -NCu admin
