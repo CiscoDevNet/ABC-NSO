bcb1da2d25ee26910c06d234af438a403f7cbf30
Joe Clarke <jclarke@cisco.com>
2020-10-01 15:05:00 -0400

    Cleanup volumes, too.

--- common/init-7-docker.sh
+++ common/init-7-docker.sh
@@ -2,6 +2,7 @@
 
 echo "Pruning Docker system"
 docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
+docker rm -f $(docker ps -aq)
 docker rmi $(docker images -q)
+docker volume rm $(docker volume ls -q)
 docker system prune -a -f
