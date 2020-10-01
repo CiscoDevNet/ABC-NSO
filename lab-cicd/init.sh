2827410f31f1071d64d18cda65e32e2067bc2720
Joe Clarke <jclarke@cisco.com>
2020-10-01 14:44:19 -0400

    Fix some ordering.

--- lab-cicd/init.sh
+++ lab-cicd/init.sh
@@ -1,14 +1,15 @@
 echo "Installing required modules"
 pip install pyats genie ncclient requests
 
-echo "Spinning up GitLab-CE"
-cd gitlab
-make
-cd ..
-
 echo "Creating nso-iac directory"
 mkdir -p ${HOME}/nso-iac
 cp -r ${LAB}/init/* ${HOME}/nso-iac
 
 init_common
+
+echo "Spinning up GitLab-CE"
+cwd=$(pwd)
+cd ${LAB}/gitlab
+make
+cd ${cwd}
 
