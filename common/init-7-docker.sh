8bb86614b486a63abb3bbdad74ab4046fdabdcb3
Joe Clarke <jclarke@cisco.com>
2020-10-02 15:21:21 -0400

    Quiet this output.

--- common/init-7-docker.sh
+++ common/init-7-docker.sh
@@ -1,8 +1,8 @@
 # Prune the Docker system
 
 echo "Pruning Docker system"
-docker stop $(docker ps -aq)
-docker rm -f $(docker ps -aq)
-docker rmi $(docker images -q)
-docker volume rm $(docker volume ls -q)
-docker system prune -a -f
+docker stop $(docker ps -aq) >/dev/null 2>&1 || exit 0
+docker rm -f $(docker ps -aq) >/dev/null 2>&1 || exit 0
+docker rmi $(docker images -q) >/dev/null 2>&1 || exit 0
+docker volume rm $(docker volume ls -q) >/dev/null 2>&1 || exit 0
+docker system prune -a -f >/dev/null 2>&1 || exit 0
