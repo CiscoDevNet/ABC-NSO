91e9eefa784980728660ac85783916754ff000c9
Joe Clarke <jclarke@cisco.com>
2020-10-02 09:35:47 -0400

    Fix some typos.
    Also make sure we don't use the repo for files that will change.

--- lab-mdt/init.sh
+++ lab-mdt/init.sh
@@ -1,7 +1,11 @@
+echo "Copying TIG bootstrap files"
+mkdir -p ${HOME}/tig
+cp -r ${LAB}/tig/* ${HOME}/tig
+
 init_common
 
 echo "Setting up TIG stack in Docker"
 cwd=$(pwd)
-cd ${LAB}/tig
+cd ${HOME}/tig
 docker-compose up -d
 cd ${cwd}
