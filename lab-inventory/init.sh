d2a1c3f7cf88e628ceae6677704084d5c2a431e9
aidevnet <ailiesiu@cisco.com>
2020-10-14 21:29:19 -0700

    small edit for init.sh in lab-inventory

--- lab-inventory/init.sh
+++ lab-inventory/init.sh
@@ -5,4 +5,4 @@ ncs_load -l -m -u admin -p '/devices/device{dist-rtr01}/config/domain' cdb2/01-o
 ncs_load -l -m -n -p '/devices/device{dist-rtr01}' cdb2/02-devices.xml
 
 cp -f xr-snippet.txt $LABDIR/
-cp -f lab2.py $LABDIR/
+cp -f lab1.py $LABDIR/
